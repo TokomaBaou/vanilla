cf52567f0b229c04337dfc13140adbb82a7ac72f
Macart <86038701+TokomaBaou@users.noreply.github.com>
2022-07-07 23:53:31 +0900

    クラス　新機能

--- src/main.js
+++ src/main.js
@@ -19,6 +19,25 @@ export const top = () => {
   console.log(human1.age);
 };
 
+// 一度「田中」の文字列を渡したら、クラスの外から書き換えたくない場合、上記のようなパブリックなフィールドでは不適切になります。
+//新機能では、プライベートなフィールドとメソッドのため、クラス外からはアクセスできなくなる
+class MyClass {
+  // プライベートなフィールド
+  #name;
+
+  constructor(name) {
+    this.#name = name;
+  }
+
+  hello() {
+    console.log(`こんにちは${this.#name}さん！`);
+  }
+}
+
+const foo = new MyClass("田中");
+foo.hello(); // 「こんにちは田中さん！」と出力される
+console.log(foo.#name); // エラー
+
 await new Promise((resolve) => {
   setTimeout(() => {
     alert("1秒経ちました");
